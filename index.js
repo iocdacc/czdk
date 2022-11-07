bc5edb585a59ea72a1fd84a1a006ea623d87700e
iocdacc <iocdacc@gmail.com>
2022-11-07 09:59:52 +0800

    2022年11月7日 09:59:56

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const app = express();
 
+app.use('/map-api', createProxyMiddleware({
+  target: 'http://175.6.136.216:6080/',
+  changeOrigin: true
+}));
+
 app.use('/', createProxyMiddleware({
   target: 'http://113.240.253.3:888/',
   changeOrigin: true
