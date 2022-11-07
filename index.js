f61fda5d72e49c0364a064833a6eefac162ceb0d
iocdacc <iocdacc@gmail.com>
2022-11-07 10:52:30 +0800

    2022年11月7日 10:52:34

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const app = express();
 
 app.use('/arcgis', createProxyMiddleware({
-  target: 'http://175.6.136.216:6080/arcgis',
+  target: 'http://175.6.136.216:6080',
   changeOrigin: true
 }));
 
 app.use('/api', createProxyMiddleware({
-  target: 'http://113.240.253.3:888/api',
+  target: 'http://113.240.253.3:888',
   changeOrigin: true
 }));
 
